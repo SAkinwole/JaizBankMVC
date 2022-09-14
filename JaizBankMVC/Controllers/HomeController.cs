3c35b837a0e9385f2a054a00246cbb3ab8c59e40
DevFlamez <samuelakinwole@gmail.com>
2022-09-14 08:07:32 +0100

    Updated Logo

--- JaizBankMVC/Controllers/HomeController.cs
+++ JaizBankMVC/Controllers/HomeController.cs
@@ -11,11 +11,51 @@ namespace JaizBankMVC.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
+            Transaction transaction = new Transaction()
+            {
+                TransactionID = 1,
+                MerchantName = "Lizpee Global Resources",
+                Location = "",
+                TerminalID = 1,
+                STAN = 003862,
+                DateTime = DateTime.Now,
+                Amount = "NGN0.01",
+                DebitCardNo = "539941*******3399",
+                ExpiryDate = "11/24",
+                AuthorizationCode = 00000,
+                Offline = "CVM",
+                ResponseCode = 91,
+                AID = "A0000000000000041010",
+                RRN = "000210002450",
+                Accelerex = "2.00-090921-LINT",
+                PTAD = "Global Accelerex"
+            };
         }
 
         public IActionResult Index()
         {
-            return View();
+            var jaizBankView = new Transaction()
+            {
+                TransactionID = 1,
+                MerchantName = "LIZPEE GLOBAL RESOURCES",
+                Location = "NO, 174, NTA/MBOURA RD, PORTHARCOURT",
+                TerminalID = 23011845,
+                STAN = 003862,
+                DateTime = DateTime.Now,
+                Amount = "NGN0.01",
+                DebitCardNo = "539941*******3399",
+                ExpiryDate = "11/24",
+                AuthorizationCode = 000000,
+                Offline = "CVM",
+                ResponseCode = 91,
+                AID = "A0000000000000041010",
+                RRN = "000210002450",
+                Accelerex = "2.00-090921-LINT",
+                PTAD = "Global Accelerex"
+            };
+
+            return View(jaizBankView); 
         }
 
         public IActionResult Privacy()
--- JaizBankMVC/Models/Transaction.cs
+++ JaizBankMVC/Models/Transaction.cs
@@ -5,6 +5,29 @@ namespace JaizBankMVC.Models
     public class Transaction
     {
         public int TransactionID { get; set; }
-        public TransactionStatus TransactionStatus { get; set; }
+
+        //Merchant
+        public string? MerchantName { get; set; }
+        public string? Location { get; set; }
+        public int TerminalID { get; set; }
+
+
+        //Purchase
+        public int STAN { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.UtcNow;
+        public string? Amount { get; set; }
+        public string? DebitCardNo { get; set; }
+        public string? ExpiryDate { get; set; }
+        public int AuthorizationCode { get; set; }
+        public string? Offline { get; set; }
+
+        //Transaction Declined
+        public int ResponseCode { get; set; }
+        public string? AID { get; set; }
+        public string? RRN { get; set; }
+
+        public string? Accelerex { get; set; }
+
+        public string? PTAD { get; set; }
     }
 }
